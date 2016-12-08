7369f8749f8050052bafc70d2ac5f35ae00bf433
Pietro Calzini <pietro.calzini@kcl.ac.uk>
2016-12-08 16:15:47 +0000

    restructured MVC pattern and one entry point for the app, added controller

--- desert/src/main/java/controller/GraphController.java
+++ desert/src/main/java/controller/GraphController.java
@@ -1,6 +1,30 @@
 package main.java.controller;
 
-public class GraphController{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import main.java.view.MainView;
+
+/**
+ * The controller of the graph view
+ * @author pietrocalzini
+ *
+ */
+public class GraphController implements EventHandler{
+
+//View
+private MainView mainView;
+
+	public GraphController(MainView m){
+		mainView = m;
+	}
 
 
+	@Override
+	public void handle(Event event) {
+		// TODO Auto-generated method stub
+		System.out.println(mainView);
+	}
 }
