48365aeafd098cd981203f71ce7413bdab3653f4
Vladislavs Uljanovs <vladislavs.uljanovs@kcl.ac.uk>
2016-12-09 01:45:40 +0000

    adding ability to convert country/indicator name or code to opposite format

--- desert/src/main/java/api/Country.java
+++ desert/src/main/java/api/Country.java
@@ -3,47 +3,89 @@ package main.java.api;
 import java.util.Locale;
 
 /**
- * Converts country name to country code and validates if such country exists.
+ * Converts country name or country code to opposite format
+ * and validates if such country exists.
  * 
  * @author Vladislavs Uljanovs
  */
 public class Country {
 	
 	// a list of all 2-letter country codes defined in ISO 3166
-	private static String[] isoCountries = Locale.getISOCountries();
+	private static String[] countries = Locale.getISOCountries();
 	
 	/**
      * Converts country name to 2-letter country code defined in ISO 3166
+     * e.g. country name "Latvia" converts to country code "LV"
      * 
      * @param 	countryName		country name to be converted to country code
-     * @return 	countryCode 	2-letter country code (ISO 3166) or null if country not found
+     * @return 				 	2-letter country code or null if country not found
      */
 	public static String getCountryCode(String countryName) {
-		String countryCode = null;
-		for (String country : isoCountries) {
-			Locale locale = new Locale("en", country);
-			if (countryName == locale.getDisplayCountry()) {
-				countryCode = country;
-				break;
-			}
+		for (String code : countries) {
+			if (countryName.toLowerCase().equals(new Locale("en", code).getDisplayCountry().toLowerCase())) 
+				return code;
 		}
-		return countryCode;
+		return null;
     }
+	
+	/**
+     * Converts 2-letter country code defined in ISO 3166 to country name
+     * e.g. country code "LV" converts to country name "Latvia"
+     * 
+     * @param 	countryCode 	2-letter country code to be converted to country name
+     * @return 					country name or null if country not found
+     */
+	public static String getCountryName(String countryCode) {
+		for (String code : countries) {
+			if (countryCode.toLowerCase().equals(code.toLowerCase())) 
+				return new Locale("en", code).getDisplayCountry();
+		}
+		return null;
+	}
 
 	/**
-	 * Gets all the valid Country names as an Array.
+	 * TODO TEMP JAVADOCS:
+     * 2 in 1 method, this method can replace the getCountryName and getCountryCode.
+     * Simply, input country code OR country name to convert to opposite.
+     * This eliminates the code duplication.
+     * 
+     * @param 	country		country code OR country name to be converted to opposite	
+     * @return 				country code OR country name depending what was inputed
+     */
+	public static String convert(String country) {
+		for (String code : countries) {
+			Locale locale = new Locale("en", code);
+			if (country.length() == 2) {
+				if (country.toLowerCase().equals(code.toLowerCase())) 
+					return locale.getDisplayCountry();
+			} else {
+				if (country.toLowerCase().equals(locale.getDisplayCountry().toLowerCase())) 
+					return code;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns a list of all the valid country names as an array.
 	 *
-	 * @return 	String array of all valid Countries.
+	 * @return countries	list of all valid countries
 	 */
 	public static String[] getCountries() {
-		String[] countryNamesArray = new String[isoCountries.length];
-
-		for(int i = 0; i < isoCountries.length; ++i) {
-			String countryCode = isoCountries[i];
-			Locale locale = new Locale("en", countryCode);
-			countryNamesArray[i] = locale.getDisplayCountry();
+		String[] countryList = new String[countries.length];
+		for (int countryCode = 0; countryCode < countryList.length; ++countryCode) {
+			Locale locale = new Locale("en", countries[countryCode]);
+			countryList[countryCode] = locale.getDisplayCountry();
 		}
-
-		return countryNamesArray;
+		return countryList;
 	}
+	
+	// EXAMPLE:
+//	public static void main(String[] args) {
+//		System.out.println(getCountryCode("LaTvia"));
+//		System.out.println(getCountryName("lV"));
+//		System.out.println(convert("LatVia"));
+//		System.out.println(convert("Lv"));		
+//	}
+	
 }
--- desert/src/main/java/api/IndicatorCodes.java
+++ desert/src/main/java/api/IndicatorCodes.java
@@ -2,14 +2,16 @@ package main.java.api;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
- * This class stores the Indicator names and its query.
+ * Converts indicator name or indicator code to opposite format
+ * and validates if such indicator exists.
  *
  * @author Haaris Memon
  */
 public class IndicatorCodes {
-
+    
     private static Map<String, String> indicatorQueryMap;
     static {
         indicatorQueryMap = new HashMap<>();
@@ -27,22 +29,41 @@ public class IndicatorCodes {
         indicatorQueryMap.put("Current Account Balance", "BN.CAB.XOKA.CD");
         indicatorQueryMap.put("Current Account Balance Percent Of GDP", "BN.CAB.XOKA.GD.ZS");
     }
-
+    
     /**
-     * Takes Indicator in english words and gives the Indicator code.
-     * For example "gdp" is passed in and "NY.GDP.MKTP.KD.ZG" is returned.
+     * Converts indicator name to indicator code.
+     * e.g. indicator name "GDP" converts to indicator code "NY.GDP.MKTP.KD.ZG"
      *
-     * @param searchIndicator - The Indicator in english words from the search
-     * @return The Indicator Code used inside the query method.
+     * @param indicatorName     indicator name to be converted to indicator code
+     * @return                  indicator code or null if indicator not found
      */
-    public static String getIndicatorCode(String searchIndicator) {
-        for(String indicator : indicatorQueryMap.keySet()) {
-            if(indicator.toLowerCase().equals(searchIndicator.toLowerCase())) {
-                return indicatorQueryMap.get(indicator);
-            }
+    public static String getIndicatorCode(String indicatorName) {
+        for (String name : indicatorQueryMap.keySet()) {
+            if (name.toLowerCase().equals(indicatorName.toLowerCase())) 
+                return indicatorQueryMap.get(name);
         }
-
         return null;
     }
+    
+    /**
+     * Converts indicator code to indicator name.
+     * e.g. indicator code "NY.GDP.MKTP.KD.ZG" converts to indicator name "GDP"
+     *
+     * @param indicatorCode     indicator code to be converted to indicator name
+     * @return                  indicator name or null if indicator not found
+     */
+    public static String getIndicatorName(String indicatorCode) {
+        for (Entry<String, String> entry : indicatorQueryMap.entrySet()) {
+            if (entry.getValue().toLowerCase().equals(indicatorCode.toLowerCase())) 
+                return entry.getKey().toString();
+        }
+        return null;
+    }
+
+    // EXAMPLE:
+//  public static void main(String[] args) {
+//      System.out.println(getIndicatorCode("GdP"));
+//      System.out.println(getIndicatorName("NY.GdP.MkTP.kd.ZG"));
+//  }
 
 }
