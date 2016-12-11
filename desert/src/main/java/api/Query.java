c154748d90a24f2f29fb4e6ae622abb6186a6de2
Vladislavs Uljanovs <vladislavs.uljanovs@kcl.ac.uk>
2016-12-11 22:37:47 +0000

    package missing fix

--- desert/src/main/java/api/Query.java
+++ desert/src/main/java/api/Query.java
@@ -1,4 +1,4 @@
-
+package main.java.api;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
--- desert/src/main/java/api/WorldBankAPI.java
+++ desert/src/main/java/api/WorldBankAPI.java
@@ -1,4 +1,4 @@
-
+package main.java.api;
 
 import java.io.BufferedReader;
 import java.io.IOException;
