3cb8257c871ff6257bebf62d24cb1c329195ce3d
Vladislavs Uljanovs <vladislavs.uljanovs@kcl.ac.uk>
2016-12-12 13:29:57 +0000

    update the query startYear and endYear method in CacheAPI, add equals for Query class

--- desert/src/main/java/api/CacheAPI.java
+++ desert/src/main/java/api/CacheAPI.java
@@ -1,6 +1,7 @@
 package main.java.api;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -74,7 +75,6 @@ public class CacheAPI {
             String line = null;
             while ((line = reader.readLine()) != null) {
                 String[] values = line.split("/");
-                
                 if (values[0].equalsIgnoreCase(query.getIndicatorCode()) 
                         && values[1].equalsIgnoreCase(query.getCountryCode()) ) { // countryCode AND indicator FOUND
                     if (!isExpired(query)) rawData = values[5];
@@ -103,14 +103,17 @@ public class CacheAPI {
             System.out.println("=> Log.fetchOffline: DATA FOUND IN CACHE");
             return false;
         } else {
-            System.out.println("=> Log.fetchOffline: DATA FOUND IN CACHE BUT OUTDATED");
-            try {
-                deleteQuery(query.getIndicatorCode(), query.getCountryCode()); // delete outdated data from cache file
-            } catch (IOException e){
-
-            }
-            return true;
+        	System.out.println("=> Log.fetchOffline: DATA FOUND IN CACHE BUT OUTDATED");
+        	if (!WorldBankAPI.connectionPresent()) {
+        		System.out.println("=> Log.fetchOffline: CONNECTION IS PRESENT SO DATA FOUND WILL BE DELETED");
+                deleteOutdatedQuery(query);
+                return true;
+        	}
         }
+        //TODO
+        System.out.println("//////////!!!!!!!!!!! INFORM THE USER !!!!!!!!!!!!\\\\\\\\\\\\");
+        System.out.println("=> Log.fetchOffline: CONNECTION IS !!NOT!! PRESENT SO USE OUTDATED QUERY");
+		return false;
     }
 
     /**
@@ -130,8 +133,7 @@ public class CacheAPI {
             return 0;
         }
     }
-    
-    // TODO add warning to the user?
+
     /**
      * Checks if number of cached queries is equal to 30 records.
      * This methods makes sure the cache file is not too big.
@@ -140,6 +142,8 @@ public class CacheAPI {
      */
     private static void checkLimit() {
         if (cacheSize() == 30) { // limit of 30 queries to be store in the cache file
+        	//TODO
+        	System.out.println("//////////!!!!!!!!!!! INFORM THE USER THAT THE query WILL BE DELETED BECAUSE THE CACHE LIMIT REACHED, THIS IS FORCED AND THE USER HAVE NO CHOICE !!!!!!!!!!!!\\\\\\\\\\\\");
             deleteOldestQuery();
         }
     }
@@ -155,53 +159,57 @@ public class CacheAPI {
             reader.close();
             deleteQuery(values[0], values[1]); // delete the oldest query i.e. first
         } catch (IOException e) {
-            
+        	System.out.println("=> Log.deleteOldestQuery: ERROR");
         }
     }
     
-    // TODO shorten this method
+    /**
+     * Deletes the out-dated query in the cache
+     * i.e. a query which not queried in the same month and year
+     * 
+     */
+    private static void deleteOutdatedQuery(Query query) {
+    	deleteQuery(query.getIndicatorCode(), query.getCountryCode());
+    }
+    
+    // TODO check that here there is no need to check if the cache null or empty
     /**
      * Deletes specified query from the cache file.
      *
      * @param indicatorCode     indicator code of the query
      * @param countryCode       country code of the query
      */
-    private static void deleteQuery(String indicatorCode, String countryCode) throws IOException {
-        File cache = getCacheFile();
-        if (cache == null) System.out.println("=> Log.deleteQuery: ERROR IS GOOD, JUST NEED TO HANDLE THIS TODO");
-        File temp = new File("temp.txt");
+    protected static void deleteQuery(String indicatorCode, String countryCode) {
+    	try {
+    		File cache = getCacheFile(); 
+    		File temp = new File("temp.txt");
 
-        BufferedReader reader = new BufferedReader(new FileReader(cache));
-        PrintWriter writer = new PrintWriter(new FileWriter(temp, true));
+            BufferedReader reader1 = new BufferedReader(new FileReader(cache));
+            PrintWriter writer1 = new PrintWriter(new FileWriter(temp, true));
 
-        String line = null;
+            String line = null;
+            while ((line = reader1.readLine()) != null) {
+                String[] values = line.split("/");
+                if (values[0].equalsIgnoreCase(indicatorCode) 
+                		&& values[1].equalsIgnoreCase(countryCode)) continue; // countryCode AND indicatorCode FOUND, skip
+                writer1.println(line);
+            }
+            writer1.close(); reader1.close();
+            
+            // copy updated list from temp file to cache file, cache file is overridden
+            BufferedReader reader2 = new BufferedReader(new FileReader(temp));
+            PrintWriter writer2 = new PrintWriter(new FileWriter(cache));
+            String tempLine = null;
+            while ((tempLine = reader2.readLine()) != null) writer2.println(tempLine);
+            writer2.close(); reader2.close();
+            
+            temp.delete();
 
-        while ((line = reader.readLine()) != null) {
-            String[] values = line.split("/");
-            // countryCode AND indicator FOUND
-            if (values[0].equalsIgnoreCase(indicatorCode) && values[1].equalsIgnoreCase(countryCode)) continue;
-            writer.println(line);
-        }
-        writer.close();
-        reader.close();
-
-        // create source as the temp file
-        BufferedReader tempReader = new BufferedReader(new FileReader(temp));
-        // create the destination to be the cache file
-        PrintWriter cacheWriter = new PrintWriter(new FileWriter(cache));
-
-        String tempLine = null;
-
-        // copy everything from temp into cache text file
-        while ((tempLine = tempReader.readLine()) != null) {
-            cacheWriter.println(tempLine);
-        }
-        cacheWriter.close();
-        tempReader.close();
-
-        temp.delete();
-
-        System.out.println("=> Log.deleteQuery: QUERY REMOVED");
+            System.out.println("=> Log.deleteQuery: QUERY REMOVED");
+    	} catch (IOException e) {
+    		System.out.println("=> Log.deleteQuery: ERROR");
+    	}
+        
     }
 
     /**
@@ -214,7 +222,64 @@ public class CacheAPI {
     }
     
     public static void updateCache(Query query) {
-    	// TODO
+    	try {
+    		File cache = getCacheFile(); 
+    		File temp = new File("temp.txt");
+
+            BufferedReader reader1 = new BufferedReader(new FileReader(cache));
+            PrintWriter writer1 = new PrintWriter(new FileWriter(temp, true));
+
+            String line = null;
+            while ((line = reader1.readLine()) != null) {
+                String[] values = line.split("/");
+                if (values[0].equalsIgnoreCase(query.getIndicatorCode()) 
+                		&& values[1].equalsIgnoreCase(query.getCountryCode())) {
+//                	System.out.println("FOUND " + values[0] + values[1]);
+//                	System.out.println(line);
+                	String newStartYear = Integer.toString(query.getStartYear());
+                	String newEndYear = Integer.toString(query.getEndYear());
+//                	System.out.println("NEW " + newStartYear);
+//                	System.out.println("NEW " + newEndYear);
+                	line = line.replace(values[2], newStartYear);
+                	line = line.replace(values[3], newEndYear);
+                	System.out.println(line);
+                }//TODO
+                writer1.println(line);
+            }
+            writer1.close(); reader1.close();
+            
+            // copy updated list from temp file to cache file, cache file is overridden
+            BufferedReader reader2 = new BufferedReader(new FileReader(temp));
+            PrintWriter writer2 = new PrintWriter(new FileWriter(cache));
+            String tempLine = null;
+            while ((tempLine = reader2.readLine()) != null) writer2.println(tempLine);
+            writer2.close(); reader2.close();
+            
+            temp.delete();
+
+            System.out.println("=> Log.deleteQuery: QUERY UPDATED");
+    	} catch (IOException e) {
+    		System.out.println("=> Log.deleteQuery: ERROR");
+    	}
+    	
+    	
+    	// Open a temporary file to write to.
+//    	PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("d:\\book.temp")));
+//
+//    	// ... then inside your loop ...
+//    	while ((line = br.readLine()) != null) {
+//    	    if (request.getParameter("hname").equals(line)) {
+//    	        line = line.replace(request.getParameter("hname"), request.getParameter("book"));
+//    	    }
+//    	    // Always write the line, whether you changed it or not.
+//    	    writer.println(line);
+//    	}
+//
+//    	// ... and finally ...
+//
+//    	File realName = new File("d:\\book.txt");
+//    	realName.delete(); // remove the old file
+//    	new File("d:\\book.temp").renameTo(realName); // Rename temp file
     }
 
     /**
@@ -242,9 +307,12 @@ public class CacheAPI {
         return queries;
     }
 
-//  public static void main(String[] args) {
-//  Query query = new Query("indicator code", "GB", 5430, 2015, new Date());
-//  System.out.println("CURRENT QUERY -> " + query);
-//  cache(query, "JSOOOOOOOON");
-//  }
+    public static void main(String[] args) throws IOException {
+    	Query q = new Query("NY.GDP.MKTP.KD.ZG", "GB", 1990, 2001, new Date());
+    	System.out.println(cacheSize());
+    	updateCache(q);
+    	System.out.println(cacheSize());
+    	
+    	
+    }
 }
