cc00d5dd1895e004c46910af3bc4896867bc9c83
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-12-12 13:31:58 +0000

    Update SearchField to incorporate Indicator suggestion

--- desert/src/main/java/view/SearchField.java
+++ desert/src/main/java/view/SearchField.java
@@ -1,28 +1,157 @@
 package main.java.view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import main.java.api.Country;
+import main.java.api.Indicator;
+import main.java.api.Query;
+import main.java.nlp.InputAnalysis;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * This class represents the search field 
  * @author pietrocalzini
  */
-public class SearchField extends TextField {
-	
+public class SearchField extends BorderPane {
+
+	private ComboBox comboBox;
+
 	/**
 	 * Create a custom search field
 	 */
 	public SearchField(){
 		super();
-		
+
+
+		comboBox = new ComboBox();
+		comboBox.setPrefSize(540, 55);
+		comboBox.setPromptText("GDP in Italy between 2010 to 2015");
+		comboBox.setFocusTraversable(false);
+		comboBox.setEditable(true);
+		for(String s : Indicator.getAllNames()) {
+			comboBox.getItems().add(s);
+		}
+
+		TextField textField = new TextField();
+		textField.setPrefSize(540, 55);
+
+		Pane pane = new Pane(comboBox);
+
 		//Link CSS file search-field.css
 		getStylesheets().add("/css/search-field.css");
 		getStyleClass().add("search-field");
+
+//		setNotFound();
+
+		comboBox.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(!Indicator.getAutocomplete(comboBox.getEditor().getText()).isEmpty()) {
+					comboBox.show();
+				}
+			}
+		});
+
+		comboBox.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					String input = comboBox.getEditor().getText();
+					if(Indicator.hasIndicator(input)) {
+						textField.setText(input);
+
+						pane.getChildren().remove(comboBox);
+						pane.getChildren().add(textField);
+						textField.requestFocus();
+						textField.positionCaret(input.length());
+					}
+					else {
+						comboBox.setValue("");
+						comboBox.hide();
+						searchQuery(input);
+						updateComboBox("");
+					}
+				} catch(Exception e) {
+
+				}
+			}
+		});
+
+		comboBox.getEditor().setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				String s = comboBox.getEditor().getText();
+				if (s.equals("")) comboBox.hide();
+				else comboBox.show();
+				comboBox.getItems().removeAll(comboBox.getItems());
+
+				if (!Indicator.getAutocomplete(s).isEmpty()) {
+//					for (String string : autocompleteIndicators) {
+//						comboBox.getItems().add(string);
+//					}
+					updateComboBox(s);
+				} else {
+					comboBox.hide();
+				}
+			}
+		});
+
+		textField.setOnKeyTyped(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (textField.getText().equals("")) {
+//                    textField.clear();
+					comboBox.getEditor().clear();
+					pane.getChildren().remove(textField);
+					pane.getChildren().add(comboBox);
+				}
+			}
+
+		});
+
+
+		textField.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				String input = textField.getText();
+				textField.setText("");
+				comboBox.hide();
+				searchQuery(input);
+				updateComboBox(textField.getText());
+			}
+		});
+
+		this.setCenter(pane);
 		
-		//set placeholder text
-		setPromptText("Income in Italy between 2010 to 2015");
-		setPrefSize(540, 55);
-		setNotFound();
-		
+	}
+
+	private void updateComboBox(String input) {
+		List<String> autocompleteIndicators = Indicator.getAutocomplete(input);
+		comboBox.getItems().clear();
+		for (String string : autocompleteIndicators) {
+			comboBox.getItems().add(string);
+		}
+	}
+
+	private void searchQuery(String input) {
+		List<String> queryInfo = InputAnalysis.isValidCommand(input);
+		Query query;
+		if(queryInfo != null) {
+			String indicatorCode = Indicator.getCode(queryInfo.get(0));
+			String countryCode = Country.getCode(queryInfo.get(1));
+			int startYear = Integer.parseInt(queryInfo.get(2));
+			int endYear = Integer.parseInt(queryInfo.get(3));
+			query = new Query(indicatorCode, countryCode, startYear, endYear, new Date());
+			System.out.println(query);
+		}
 	}
 	
 	/**
