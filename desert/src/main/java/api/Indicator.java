e15037903be7774417cb7cd021f554419dd50078
Haaris Memon <haaris.memon@kcl.ac.uk>
2016-12-13 12:27:11 +0000

    Add syllabus information for each Indicator information label

--- desert/src/main/java/api/Indicator.java
+++ desert/src/main/java/api/Indicator.java
@@ -64,26 +64,32 @@ public class Indicator {
 		indicatorsNameInfo = new HashMap<>();
 
 		indicatorsNameInfo.put("GDP",
-				"Gross domestic product (GDP) is the overall market value of all the goods/services a country produces. "
+				"(Unit 2)3.2.3\nGross domestic product (GDP) is the overall market value of all the goods/services a country produces. "
 						+ "GDP is usually calculated on an Annual basis but sometimes on a quarterly basis within a year. "
-						+ "This is used to determine the economic performance of a whole country or region and to make international comparisons.");
+						+ "This is used to determine the economic performance of a whole country or region and to make international comparisons." +
+						"\nSyllabus: Unit 2 Section 3.2.3 ");
 
 		indicatorsNameInfo.put("GDP Growth",
-				"Indicates how much a country's production has increased (or decreased, if the growth rate is negative) compared to the previous year.");
+				"Indicates how much a country's production has increased (or decreased, if the growth rate is negative) compared to the previous year." +
+						"\nSyllabus: Unit 2 Section 3.2.3");
 		indicatorsNameInfo.put("GDP Per Capita",
 				"Per capita GDP is a measure of the total output of a country that takes gross domestic product (GDP) and divides it by the number of people in the country. "
 						+ "The per capita GDP is especially useful when comparing one country to another, because it shows the relative performance of the countries. "
-						+ "A rise in per capita GDP signals growth in the economy and tends to reflect an increase in productivity.");
+						+ "A rise in per capita GDP signals growth in the economy and tends to reflect an increase in productivity." +
+						"\nSyllabus: Unit 2 Section 3.2.3");
 
 		indicatorsNameInfo.put("GDP Per Capita Growth",
-				"GDP Per Capita Growth is the growth in Per Capita GDP year-on-year, often expressed a percentage increase/decrease.");
+				"GDP Per Capita Growth is the growth in Per Capita GDP year-on-year, often expressed a percentage increase/decrease." +
+						"\nSyllabus: Unit 2 Section 3.2.3");
 		indicatorsNameInfo.put("Consumer Price Inflation",
 				"Inflation is a sustained increase in the cost of living or the general price level leading to a fall in the purchasing power of money. "
-						+ "The rate of inflation is measured by the annual percentage change in consumer prices. The main measure of inflation is the consumer price index (CPI).");
+						+ "The rate of inflation is measured by the annual percentage change in consumer prices. The main measure of inflation is the consumer price index (CPI)." +
+						"\nSyllabus: Unit 4 Section 3.4.1");
 
 		String unemployment = "The national unemployment rate is defined as the percentage of unemployed workers in the total labor force. "
 				+ "It is widely recognized as a key indicator of labor market performance. "
-				+ "Unemployed workers lose their purchasing power, which can lead to unemployment for other workers, creating a cascading effect that ripples through the economy.";
+				+ "Unemployed workers lose their purchasing power, which can lead to unemployment for other workers, creating a cascading effect that ripples through the economy." +
+				"\nSyllabus: Unit 2 Section 3.2.3";
 
 		indicatorsNameInfo.put("Unemployment Total", unemployment);
 		indicatorsNameInfo.put("Unemployment Male", unemployment);
@@ -91,11 +97,15 @@ public class Indicator {
 		indicatorsNameInfo.put("Unemployment Female", unemployment);
 		indicatorsNameInfo.put("Unemployment Young Female", unemployment);
 		indicatorsNameInfo.put("GDP Deflator Inflation",
-				"A measure of inflation/deflation in an economy with respect to a specific base year. The equation is GDP deflator = Nominal GDP/Real GDP x 100");
+				"A measure of inflation/deflation in an economy with respect to a specific base year. The equation is GDP deflator = Nominal GDP/Real GDP x 100" +
+						"\nSyllabus: Unit 2 Section 3.2.3");
 		indicatorsNameInfo.put("Current Account Balance",
-				"The current account balance is comprised of the balance of trade (net exports and imports) and net cash transfers, that take place over a given year.");
+				"The current account balance is comprised of the balance of trade (net exports and imports) and net cash transfers, that take place over a given year." +
+						"\nSyllabus: Unit 2 Section 3.2.3");
 		indicatorsNameInfo.put("Current Account Balance Percent Of GDP",
-				"The current account balance as a percentage of GDP");
+				"The current account balance as a percentage of GDP" +
+						"\nSyllabus: Unit 2 Section 3.2.3");
+
 	}
 
 	/**
